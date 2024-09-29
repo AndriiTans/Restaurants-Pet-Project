08f4034820d1030e18ce59d4d2b5f2d4b50fb356
Andrii Tantsiura <andrew.tans9229@gmail.com>
2024-09-29 18:25:14 +0200

    Added ErrorHandlingMiddleware

---
+++ Restaurants.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.API.Middlewares
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (NotFoundException notFound)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFound.Message);
+
+                _logger.LogWarning(notFound.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
+
